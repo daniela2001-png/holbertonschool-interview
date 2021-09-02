928eee17bbfdd5d9f7da17e2089dc8a2bf7478c3
Daniela Morales <58857379+daniela2001-png@users.noreply.github.com>
2021-09-02 17:38:28 -0500

    Delete 0x06-log_parsing directory

--- 0x06-log_parsing/test.py
+++
@@ -1,9 +0,0 @@
-def min_maquina():
-    Xi = 1.0
-    while Xi / 2.0 > 0.0:
-        Xi = Xi / 2.0
-    return Xi
-
-
-print("El m´ınimo n´umero positivo", end=" ")
-print("en esta m´aquina es:", min_maquina())
